59ee618c047aaa48f3ed95bd681f770547b2b395
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-01-22 23:49:59 -0300

    uma app básica de lista de reprodução de áudios para usarmos no projeto

---
+++ projeto/app_exemplo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from models import Audio, ListaDeReproducao
+
+
+class AudioAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('titulo',)}
+admin.site.register(Audio, AudioAdmin)
+
+class ListaDeReproducaoAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('titulo',)}
+admin.site.register(ListaDeReproducao, ListaDeReproducaoAdmin)
--- projeto/app_exemplo/models.py
+++ projeto/app_exemplo/models.py
@@ -1,3 +1,49 @@
+# coding:utf-8
 from django.db import models
+from django.db.models import permalink
+from django.conf import settings
 
-# Create your models here.
+
+class ListaDeReproducao(models.Model):
+	"""
+	 Model de uma lista de reprodução de arquivos de áudio.
+	"""
+	titulo = models.CharField('título', max_length=255)
+	slug = models.SlugField()
+	descricao = models.TextField('descrição', blank=True)
+	audios = models.ManyToManyField('Audio')
+	criado = models.DateTimeField(auto_now_add=True)
+	modificado = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return '%s' % self.titulo
+
+	class Meta:
+		verbose_name = 'lista de reprodução'
+		verbose_name_plural = 'listas de reprodução'
+
+	# @permalink
+	# def get_absolute_url(self):
+	#     return ('audio_set_detail', None, { 'slug': self.slug })
+
+
+class Audio(models.Model):
+	"""
+	 Model de arquivos de áudio.
+	"""
+	titulo = models.CharField('título', max_length=255)
+	slug = models.SlugField()
+	audio = models.FilePathField('áudio',path=settings.MEDIA_ROOT + 'audios/', recursive=True)
+	descricao = models.TextField('descrição', blank=True)
+	criado = models.DateTimeField(auto_now_add=True)
+	modificado = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return '%s' % self.titulo
+
+	class Meta:
+		verbose_name = 'áudio'
+
+	# @permalink
+	# def get_absolute_url(self):
+	#   return ('audio_detail', None, { 'slug': self.slug })
