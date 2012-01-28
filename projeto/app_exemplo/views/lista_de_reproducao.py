6240b65355855734e9aa7b4a977cb3bd685576e0
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-01-28 12:38:49 -0300

    código para o primeiro exeplo, implementação de ListView

--- projeto/app_exemplo/views/lista_de_reproducao.py
+++ projeto/app_exemplo/views/lista_de_reproducao.py
@@ -1,1 +1,33 @@
-# Create your views here.
+from django.views.generic import ListView
+from app_exemplo.models import ListaDeReproducao
+
+
+class ListasDeReproducao(ListView):
+	"""
+	# template_name = 'blog/todos-artigos.html'
+	# context_object_name = 'listas_de_reproducao'
+	# queryset = ListaDeReproducao.objects.all()
+	# paginate_by = 4
+
+	def get_context_data(self, **kwargs):
+		context = super(ListasDeReproducao, self).get_context_data(**kwargs)
+		print context
+		return context
+
+	 {
+	 	'paginator': None,
+	 	'page_obj': None,
+	 	'is_paginated': False,
+	 	'object_list':
+	 		[<ListaDeReproducao: Animiais>,
+	 		<ListaDeReproducao: Clicks>,
+	 		<ListaDeReproducao: Armas disparando>,
+	 		<ListaDeReproducao: Sons de armas (GERAL)>],
+	 	'listadereproducao_list':
+	 		[<ListaDeReproducao: Animiais>,
+	 		<ListaDeReproducao: Clicks>,
+	 		<ListaDeReproducao: Armas disparando>,
+	 		<ListaDeReproducao: Sons de armas (GERAL)>]
+	 }
+	"""
+	model = ListaDeReproducao
--- projeto/urls.py
+++ projeto/urls.py
@@ -10,8 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'projeto.views.home', name='home'),
     # url(r'^projeto/', include('projeto.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^app-exemplo/', include('app_exemplo.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
