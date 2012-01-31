3c830a5699b8b530ad97291ead9b1d5677a8d01b
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-01-31 19:02:14 -0300

    conteúdo para o template da lista de reprodução, exemplo 1

--- projeto/app_exemplo/urls.py
+++ projeto/app_exemplo/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, url
 from views.lista_de_reproducao import ListasDeReproducao
 
 urlpatterns = patterns('',
-	url(r'^listas-de-reproducao/', ListasDeReproducao.as_view(), name='app_exemplo-listas-de-reproducao'),
+	url(r'^listas-de-reproducao/', ListasDeReproducao.as_view(), name='app_exemplo-listview-exemplo-1'),
 )
