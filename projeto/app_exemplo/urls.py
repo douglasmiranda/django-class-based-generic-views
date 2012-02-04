12f785044847272b6aef944a7dda4f88e94258da
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-02-04 10:36:51 -0300

    renomeando o arquivo de views onde estarão somente list e datail views

--- projeto/app_exemplo/urls.py
+++ projeto/app_exemplo/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, url
-from views.lista_de_reproducao import ListasDeReproducao
+from views.list_and_detail import ListasDeReproducao
 
 urlpatterns = patterns('',
 	url(r'^listas-de-reproducao/', ListasDeReproducao.as_view(), name='app_exemplo-listview-exemplo-1'),
