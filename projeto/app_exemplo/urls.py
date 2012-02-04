d3b5248ae25980684d51019a134625b7e77ee871
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-02-04 12:05:42 -0300

    mudando url da lista de reprodução

--- projeto/app_exemplo/urls.py
+++ projeto/app_exemplo/urls.py
@@ -3,5 +3,5 @@ from views.list_and_detail import ListasDeReproducao, DetalhesListasDeReproducao
 
 urlpatterns = patterns('',
 	url(r'^listas-de-reproducao/', ListasDeReproducao.as_view(), name='app_exemplo-listview-exemplo-1'),
-	url(r'^(?P<pk>\d+)/$', DetalhesListasDeReproducao.as_view(), name='app_exemplo-detailview-exemplo-1'),
+	url(r'^lista-de-reproducao/(?P<pk>\d+)/$', DetalhesListasDeReproducao.as_view(), name='app_exemplo-detailview-exemplo-1'),
 )
