ce974ecdb885edb740d85f4bb44709efdd5c0762
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-02-06 15:03:07 -0300

    conteúdo do template para exibição dos detalhes de uma lista de reprodução

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'projeto.views.home', name='home'),
-    # url(r'^projeto/', include('projeto.foo.urls')),
+    (r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
     url(r'^app-exemplo/', include('app_exemplo.urls')),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
 
