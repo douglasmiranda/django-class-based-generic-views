c8701d408a298af83d501876de3a775b4923c861
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-05-27 12:47:02 -0400

    adicionando ao urls.py a url para as json views

--- projeto/app_exemplo/urls.py
+++ projeto/app_exemplo/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 from views.list_and_detail import ListasDeReproducao, DetalhesListasDeReproducao
+from views.json import ListasDeReproducaoJson
 
 urlpatterns = patterns('',
-    url(r'^listas-de-reproducao/', ListasDeReproducao.as_view(), name='app_exemplo-listview-exemplo-1'),
-    url(r'^lista-de-reproducao/(?P<pk>\d+)/$', DetalhesListasDeReproducao.as_view(), name='app_exemplo-detailview-exemplo-1'),
+    # list_and_detail.py
+    url(r'^listas-de-reproducao/', ListasDeReproducao.as_view(), name='app_exemplo-listview'),
+    url(r'^lista-de-reproducao/(?P<pk>\d+)/$', DetalhesListasDeReproducao.as_view(), name='app_exemplo-detailview'),
+    # json.py
+    url(r'^json/listas-de-reproducao/', ListasDeReproducaoJson.as_view(), name='app_exemplo-json-listview'),
 )
